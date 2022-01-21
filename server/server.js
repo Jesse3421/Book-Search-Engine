8ba6d87ad3e56f59c028eb00f8f73738c657a3dc
JesseCodes <88518880+Jesse3421@users.noreply.github.com>
2022-01-21 17:24:24 -0600

    completing mutations for components

--- server/server.js
+++ server/server.js
@@ -23,6 +23,15 @@ const app = express();
   app.use(express.json());
   app.use(routes);
   
+  // Serve up static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
@@ -30,17 +39,5 @@ const app = express();
   });
 });
 
-// Serve up static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
