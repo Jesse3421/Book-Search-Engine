de9086f0426f737204cecece38e666550d403125
JesseCodes <88518880+Jesse3421@users.noreply.github.com>
2022-01-23 14:46:25 -0600

    troubleshooting

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://jesse3421:password1234@cluster0.dtpei.mongodb.net/BookSearchEngineDB?retryWrites=true&w=majority',
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book-search-engine',
 {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -8,4 +8,3 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://jesse3421:password123
 
 module.exports = mongoose.connection;
 
-//'mongodb://localhost:27017/book-search-engine'
