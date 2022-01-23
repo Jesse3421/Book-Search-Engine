9f2340efb426896f99ff4cd6e9331ad08c690abb
JesseCodes <88518880+Jesse3421@users.noreply.github.com>
2022-01-23 12:05:21 -0600

    deploying

--- server/server.js
+++ server/server.js
@@ -25,10 +25,10 @@ const app = express();
   
   // Serve up static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.resolve('../client/build')));
     
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/build/index.html'));
+      res.sendFile(path.resolve('../client/build/index.html'));
     });
   }
   
