131ac2bd0afa3ca8485e011c53cf141faba2a7a3
JesseCodes <88518880+Jesse3421@users.noreply.github.com>
2022-01-23 15:14:12 -0600

    working on connection to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,6 +4,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/book-sear
 {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
